d081c52d523d8c49625d90d9a26e0760bc276a51
Davíð Guðni <davidgudni@gmail.com>
2015-08-05 06:04:02 +0000

    Moved the scripts for i3 in seprate folders to make them more organized

--- makeSymLinks.sh
+++ makeSymLinks.sh
@@ -5,7 +5,7 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="astylerc dircolors fonts gvimrc i3 nvimrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="astylerc dircolors dunstrc fonts gvimrc i3 nvimrc vimrc vim zshrc oh-my-zsh xinitrc"    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in the home folder"
@@ -18,5 +18,5 @@ for file in $files; do
     echo "Moving any existing dotfiles from the home folder to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s ~/$dir/$file ~/.$file
+    ln -s $dir/$file ~/.$file
 done
