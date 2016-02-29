33190f2b070074b716a310156a57b9efd0e0a584
Davíð Guðni <davidgudni@gmail.com>
2016-02-29 00:25:04 +0000

    Created a wallpaper slideshow script

---
+++ i3/scripts/wallpaper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+NSCREENS=`xrandr | grep " connected " | wc -l`
+HD="/home/dabbeg/.i3/pictures/wallpaper/1920x1080"
+SD="/home/dabbeg/.i3/pictures/wallpaper/1280x1024"
+IMAGES=""
+
+i=0
+
+while [ $i -lt $NSCREENS ]; do
+  if [ "$i" -eq 2 ]; then
+    IMAGES="$(ls $SD/* | shuf -n 1) $IMAGES"
+  else
+    IMAGES="$(ls $HD/* | shuf -n 1) $IMAGES"
+  fi
+  i=$[$i+1]
+done
+
+convert +append $IMAGES "/tmp/wallpaper.jpg"
+feh --no-xinerama --bg-scale "/tmp/wallpaper.jpg"
