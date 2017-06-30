9f53e953f7881f6f6ba69fe64325f91c50b3ffd6
Davíð Guðni <davidgh@ru.is>
2017-06-30 11:37:18 +0000

    Updating paths of changed files

--- i3/scripts/wallpaper.sh
+++ i3/scripts/wallpaper.sh
@@ -5,28 +5,17 @@ xrandr --noprimary
 RESOLUTIONS=$(xrandr | grep " connected" | cut -d ' ' -f3)
 # Sorting resolutions according to position of screens
 RESOLUTIONS=($(echo "${RESOLUTIONS//"+"/" "}" | sort -k 2 | cut -d ' ' -f1))
-WALLPAPER="/home/dabbeg/.i3/pictures/wallpaper/"
+WALLPAPER="$HOME/.config/i3/pictures/wallpaper/"
 IMAGES=""
-
-PRIMARY=1
 
 for RESOLUTION in "${RESOLUTIONS[@]}"; do
   PATHTOIMAGES="$WALLPAPER$RESOLUTION/*"
-
-  if [ "$RESOLUTION" == "1920x1080" ] && [ "$PRIMARY" -eq 1 ]; then
-      PATHTOIMAGES="$WALLPAPER/calendar/*"
-      if [ "$IMAGES" == "" ]; then
-        IMAGES=$(ls $PATHTOIMAGES | grep `date '+%b'`)
-      else
-        IMAGES="$IMAGES $(ls $PATHTOIMAGES | grep `date '+%b'`)"
-      fi
-      PRIMARY=0
+  echo $PATHTOIMAGES
+  echo ""
+  if [ "$IMAGES" == "" ]; then
+    IMAGES=$(ls $PATHTOIMAGES | shuf -n 1)
   else
-      if [ "$IMAGES" == "" ]; then
-        IMAGES=$(ls $PATHTOIMAGES | shuf -n 1)
-      else
-        IMAGES="$IMAGES $(ls $PATHTOIMAGES | shuf -n 1)"
-      fi
+    IMAGES="$IMAGES $(ls $PATHTOIMAGES | shuf -n 1)"
   fi
 done
 
